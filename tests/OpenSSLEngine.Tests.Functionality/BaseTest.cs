78c125e2030e2b2f046283141ef3a36a5b2e8ea4
AtLeastITry <matthew.hope396@outlook.com>
2019-06-14 18:36:05 +0100

    Added some tests.

---
+++ tests/OpenSSLEngine.Tests.Functionality/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenSSLEngine.Abstraction;
+using OpenSSLEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSSLEngine.Tests.Functionality
+{
+    public class BaseTest
+    {
+        protected readonly IOpenSSLEngine _engine;
+
+        public BaseTest()
+        {
+            var services = new ServiceCollection();
+            services.AddOpenSSl();
+            var serviceProvider = services.BuildServiceProvider();
+            _engine = serviceProvider.GetRequiredService<IOpenSSLEngine>();
+        }
+    }
+}
--- tests/OpenSSLEngine.Tests.Functionality/Pscs12Tests.cs
+++ tests/OpenSSLEngine.Tests.Functionality/Pscs12Tests.cs
@@ -1,14 +1,98 @@
+using OpenSSLEngine.Abstraction.Commands.Pkcs12;
+using OpenSSLEngine.Abstraction.Commands.Req;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace OpenSSLEngine.Tests.Functionality
 {
-    public class Pscs12Tests
+    public class Pscs12Tests : BaseTest
     {
         [Fact]
-        public void Test1()
+        public async Task File_Created_Async()
         {
+            var keyFilePath = $"{Path.GetTempPath()}\\{Guid.NewGuid()}.key";
+            var cerFilePath = $"{Path.GetTempPath()}\\{Guid.NewGuid()}.cer";
+            var pfxFilePath = $"{Path.GetTempPath()}\\{Guid.NewGuid()}.pfx";
 
+            await _engine.ReqAsync(
+                options: new ReqOptions
+                {
+                    NewKey = "rsa:2048",
+                    Nodes = true,
+                    KeyOut = keyFilePath,
+                    X509 = true,
+                    Days = 365,
+                    Out = cerFilePath
+                },
+                input: new ReqInput
+                {
+                    CommonName = "",
+                    CountryName = "",
+                    EmailAddress = "",
+                    LocalityName = "",
+                    OrganizationalUnitName = "",
+                    OrganizationName = "",
+                    StateOrProvinceName = ""
+                }
+            );
+
+            await _engine.Pkcs12Async(
+                options: new Pkcs12Options
+                {
+                    Export = true,
+                    In = cerFilePath,
+                    InKey = keyFilePath,
+                    Out = pfxFilePath,
+                    PassOut = "pass:1234"
+                }
+            );
+
+            Assert.True(File.Exists(pfxFilePath));
+        }
+
+        [Fact]
+        public void File_Created()
+        {
+            var keyFilePath = $"{Path.GetTempPath()}\\{Guid.NewGuid()}.key";
+            var cerFilePath = $"{Path.GetTempPath()}\\{Guid.NewGuid()}.cer";
+            var pfxFilePath = $"{Path.GetTempPath()}\\{Guid.NewGuid()}.pfx";
+
+            _engine.Req(
+                options: new ReqOptions
+                {
+                    NewKey = "rsa:2048",
+                    Nodes = true,
+                    KeyOut = keyFilePath,
+                    X509 = true,
+                    Days = 365,
+                    Out = cerFilePath
+                },
+                input: new ReqInput
+                {
+                    CommonName = "",
+                    CountryName = "",
+                    EmailAddress = "",
+                    LocalityName = "",
+                    OrganizationalUnitName = "",
+                    OrganizationName = "",
+                    StateOrProvinceName = ""
+                }
+            );
+
+            _engine.Pkcs12(
+                options: new Pkcs12Options
+                {
+                    Export = true,
+                    In = cerFilePath,
+                    InKey = keyFilePath,
+                    Out = pfxFilePath,
+                    PassOut = "pass:1234"
+                }
+            );
+
+            Assert.True(File.Exists(pfxFilePath));
         }
     }
 }
